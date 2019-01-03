86c804f44a8c97a8ecea9c74f047b16eb9dee03b
Andrey <dewshick@gmail.com>
2019-01-03 18:39:45 +0300

    try to simplify dsl

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,11 @@ extern crate piston_window;
 use piston_window::*;
 
 const WHITE : types::Color = [1.0; 4];
-//
-//struct Forest { }
-//struct Village { }
-//struct Mine { }
 
 enum Tile { Forest, Village, Mine }
 enum Map {
 	RectTile { tile : Tile, width : u16, height : u16 },
-	TileOnTop { main: Box<Map>, background : Box<Map>, x_off : u16, y_off : u16 }
+	TileOnTop { main: Box<[(u16, u16, Map)]>, background : Box<Map> }
 }
 
 fn tile_color(t : &Tile) -> types::Color {
@@ -29,9 +25,9 @@ fn render_map<G>(x : u16, y :u16, map : &Map, t : math::Matrix2d, g : &mut G) wh
 			let rect = [x as f64, y as f64, *width as f64, *height as f64];
 			rectangle(tile_color(tile), rect, t, g)
 		},
-		Map::TileOnTop { main, background, x_off, y_off } => {
+		Map::TileOnTop { main, background } => {
 			render_map(x, y, background.as_ref(), t, g);
-			render_map(x + x_off, y + y_off, main.as_ref(), t, g)
+			main.as_ref().iter().for_each ( |(x_off, y_off, submap)| render_map(x + x_off, y + y_off, submap, t, g) );
 		}
 	}
 }
@@ -40,10 +36,11 @@ fn main() {
 	let mut window: PistonWindow =
 		WindowSettings::new("Hello Piston!", [640, 480]).exit_on_esc(true).build().unwrap();
 	let world = Map::TileOnTop {
-		main : Box::new(Map::RectTile { tile : Tile::Village, width : 100, height : 100 }),
+		main : Box::new([
+			(120, 120, Map::RectTile { tile : Tile::Village, width : 100, height : 100 }),
+			(350, 250, Map::RectTile { tile : Tile::Mine, width : 50, height : 50 }),
+		]),
 		background : Box::new(Map::RectTile { tile : Tile::Forest, width : 480, height : 320 }),
-		x_off : 120,
-		y_off : 120
 	};
 
 	while let Some(event) = window.next() {
