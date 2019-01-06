ed8830a0ba79e64ca8831584616bd9356faad7ca
Andrey <dewshick@gmail.com>
2019-01-06 21:31:50 +0300

    support trees in world rendering

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,18 @@ const WHITE : types::Color = [1.0; 4];
 fn main() {
 	let mut window: PistonWindow =
 		WindowSettings::new("Hello Piston!", [640, 480]).exit_on_esc(true).build().unwrap();
-	let world = Map::TileOnTop {
+	let world = generate_world(Map::TileOnTop {
 		main : Box::new([
 			(Point::new(120.0, 120.0), Map::RectTile { tile : Tile::Village, size : Point::new(100.0, 100.0) }),
 			(Point::new(350.0, 250.0), Map::RectTile { tile : Tile::Mine, size : Point::new(80.0, 50.0) }),
 		]),
 		background : Box::new(Map::RectTile { tile : Tile::Forest, size : Point::new(480.0, 320.0) }),
-	};
+	}, 6.0);
 
 	while let Some(event) = window.next() {
 		window.draw_2d(&event, |context, graphics| {
 			clear(WHITE, graphics);
-			render_map(Point::init(), &world, context.transform, graphics)
+			render_world(&world, context.transform, graphics)
 		});
 	}
 }
--- src/world_gen.rs
+++ src/world_gen.rs
@@ -13,9 +13,7 @@ pub enum Map {
 pub struct World { map : Map, trees : Vec<CircleBounds> }
 const TREE_WIDTH : Dist = 5.0;
 
-pub fn generate_world(map : Map, tree_dist : Dist) -> World {
-	World { map : map, trees : vec![] }
-}
+pub fn generate_world(map : Map, tree_dist : Dist) -> World { World { map, trees : vec![] } }
 
 fn tile_color(t : &Tile) -> piston_window::types::Color {
 	match t {
@@ -26,15 +24,23 @@ fn tile_color(t : &Tile) -> piston_window::types::Color {
 }
 
 // todo: check if background tile is not overlapping main tile
-pub fn render_map<G>(p : Point, map : &Map, t : piston_window::math::Matrix2d, g : &mut G) where G : piston_window::Graphics {
+fn render_map<G>(p : Point, map : &Map, t : piston_window::math::Matrix2d, g : &mut G) where G : piston_window::Graphics {
 	match map {
 		Map::RectTile { tile, size } => {
 			let rect = [p.x as f64, p.y as f64, size.x as f64, size.y as f64];
-			piston_window::rectangle(tile_color(tile), rect, t, g)
+			piston_window::rectangle(tile_color(&tile), rect, t, g)
 		},
 		Map::TileOnTop { main, background } => {
 			render_map(p, background.as_ref(), t, g);
 			main.as_ref().iter().for_each ( |(offset, submap)| render_map(*offset + p, submap, t, g) );
 		}
 	}
+}
+
+pub fn render_world<G>(world : &World, t : piston_window::math::Matrix2d, g : &mut G) where G : piston_window::Graphics  {
+	render_map(Point::init(), &world.map, t, g);
+	world.trees.iter().for_each( |tree| {
+		let (upper, side) = ((tree.coords.x - tree.r) as f64, tree.r as f64);
+		piston_window::ellipse(solid_color(Brown), [upper, upper, side, side], t, g);
+	})
 }
